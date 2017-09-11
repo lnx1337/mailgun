a20ebf8e5c66374f3252001a25ae5a756d23451b
Jair Peña <dev.lnx1337@gmail.com>
2017-09-10 19:36:47 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Mailgun.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [{:exvcr, "~> 0.4.0", only: [:test]},
-     {:poison, "~> 2.1"}
+     {:poison, "~> 2.3"}
     ]
   end
 end
